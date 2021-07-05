6d9496c160de436eee056d47231798320635b8cb
choi-dog <52483125+choi-dog@users.noreply.github.com>
2021-07-05 16:24:03 +0800

    change the file folder name from 'third_weeks' to 'third_week', add fouth_week file folder

---
+++ fourth_week/js/script.js
@@ -0,0 +1,45 @@
+//Lecture 41
+
+// var message = "in global";
+// console.log("global: message = " + message);
+
+// var a = function(){
+//     var message = "inside a";
+//     console.log("a: message = " + message);
+//     b();
+// }
+
+// function b(){
+//     console.log("b: message = " + message);
+// }
+// a();
+
+
+//Lecture 42
+
+// var x;
+// console.log(x);
+
+// if (x == undefined) {
+//     console.log("x is undefined");
+// }
+
+// x=5;
+// if (x == undefined) {
+//     console.log("x is undefined");
+// } else {
+//     console.log("X has been defined");
+// }
+
+// IF statement (all false)
+// if (false || null || undefined || "" || 0 || NaN) {
+//     console.log("This line won't ever executer");
+// } else {
+//     console.log("All false");
+// }
+
+// // IF statement (all true)
+// if (true && "hello" && 1 && -1 && "false") {
+//     console.log("All true");
+// }
+
